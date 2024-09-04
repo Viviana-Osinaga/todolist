d24d3a043d2323de63bc42844f0e20102c1ef8e9
viviana <vivianasolizosinaga@gmail.com>
2024-09-04 02:59:38 -0300

    column creation

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-db =SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)  #ERROR!
+
+    def __repr__(self):
+        return '<Task %r>' % self.id    #when create new element, it resturns the task and the id of the task created
+
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+        # with app.app_context():
+        #     db.create_all  
 
 if __name__ == "__main__":
     app.run(debug=True)
