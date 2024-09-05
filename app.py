31267869fa9ba612a0fba64421a94cb72cc271bb
viviana <vivianasolizosinaga@gmail.com>
2024-09-05 00:31:43 -0300

    creating get, and post, adding stiles

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)  #ERROR!
+    # completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now().replace(microsecond=0))
 
     def __repr__(self):
         return '<Task %r>' % self.id    #when create new element, it resturns the task and the id of the task created
-
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -34,8 +34,9 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
     
-        # with app.app_context():
-        #     db.create_all  
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
